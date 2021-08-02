331210714f5f57f5c96daff62c12364e6a276cee
MohamedGamal22206 <muhammedgammal206@gmail.com>
2021-08-02 22:33:34 +0200

    fix issues

--- src/app/fishing-trip/fishing-trip.module.ts
+++ src/app/fishing-trip/fishing-trip.module.ts
@@ -5,15 +5,17 @@ import { FishingTripRoutingModule } from './fishing-trip-routing.module';
 import {FishingTripComponent} from './fishing-trip.component';
 import {NgbCarouselModule} from '@ng-bootstrap/ng-bootstrap';
 import {SharedModule} from '../../shared/shared.module';
+import {TranslateModule} from '@ngx-translate/core';
 
 
 @NgModule({
   declarations: [FishingTripComponent],
-  imports: [
-    CommonModule,
-    FishingTripRoutingModule,
-    NgbCarouselModule,
-    SharedModule
-  ]
+    imports: [
+        CommonModule,
+        FishingTripRoutingModule,
+        NgbCarouselModule,
+        SharedModule,
+        TranslateModule
+    ]
 })
 export class FishingTripModule { }
--- src/app/main-destination/destination.module.ts
+++ src/app/main-destination/destination.module.ts
@@ -4,14 +4,16 @@ import { CommonModule } from '@angular/common';
 import { DestinationRoutingModule } from './destination-routing.module';
 import {MainDestinationComponent} from './main-destination.component';
 import {SharedModule} from '../../shared/shared.module';
+import {TranslateModule} from '@ngx-translate/core';
 
 
 @NgModule({
   declarations: [MainDestinationComponent],
-  imports: [
-    CommonModule,
-    DestinationRoutingModule,
-    SharedModule
-  ]
+    imports: [
+        CommonModule,
+        DestinationRoutingModule,
+        SharedModule,
+        TranslateModule
+    ]
 })
 export class DestinationModule { }
