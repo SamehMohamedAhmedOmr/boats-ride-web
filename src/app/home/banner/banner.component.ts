5b15ea4a6dc762e30a685d728734316dbc0e2695
Sameh Mohamed Ahmed Omr <Conan_Sameh@yahoo.com>
2021-09-15 21:00:32 +0200

    build v 3.1

--- src/app/home/banner/banner.component.ts
+++ src/app/home/banner/banner.component.ts
@@ -12,7 +12,7 @@ export class BannerComponent implements OnInit {
   // images = [1055, 194, 368].map((n) => `https://picsum.photos/id/${n}/900/500`);
   images = [
     {url:"assets/Banners/new/fishing.jpg",titleEn:"Fishing Trip",navigate:'/fishing-trip',titleAr:"رحلات صيد السمك"},
-    {url:"assets/Banners/services-01-01.jpg",titleEn:"Services",navigate:'/services',titleAr:"الخدمات"},
+    {url:"assets/Banners/new/services.png",titleEn:"Services",navigate:'/services',titleAr:"الخدمات"},
     {url:"assets/Banners/new/water-sport.png",titleEn:"Water Sports",navigate:'/water-sports',titleAr:"العاب الماء"},
     {url:"assets/Banners/new/offer.jpg",titleEn:"Offers",navigate:'/offers',titleAr:"عروض"},
     {url:"assets/Banners/new/yachts-home.png",titleEn:"Yacht Rental",navigate:'/yachts',titleAr:"تأجير اليخوت"},
