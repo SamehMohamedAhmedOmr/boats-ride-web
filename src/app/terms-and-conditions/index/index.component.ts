eefbc877757c87600bd03ccd136f8c5be4d61976
Sameh Mohamed Ahmed Omr <Conan_Sameh@yahoo.com>
2021-09-24 01:46:22 +0200

    terms and conditions

--- src/app/terms-and-conditions/index/index.component.ts
+++ src/app/terms-and-conditions/index/index.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-index',
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexComponent implements OnInit {
 
-  constructor() { }
+  lang = localStorage.getItem('lang');
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
--- src/app/terms-and-conditions/terms-and-conditions.module.ts
+++ src/app/terms-and-conditions/terms-and-conditions.module.ts
@@ -6,13 +6,15 @@ import { TermsAndConditionsComponent } from './terms-and-conditions.component';
 import { IndexComponent } from './index/index.component';
 import { IndexArComponent } from './index/index-ar/index-ar.component';
 import { IndexEnComponent } from './index/index-en/index-en.component';
+import {TranslateModule} from "@ngx-translate/core";
 
 
 @NgModule({
   declarations: [TermsAndConditionsComponent, IndexComponent, IndexArComponent, IndexEnComponent],
   imports: [
     CommonModule,
-    TermsAndConditionsRoutingModule
+    TermsAndConditionsRoutingModule,
+    TranslateModule
   ]
 })
 export class TermsAndConditionsModule { }
