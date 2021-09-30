e62c3651914aacee7362bd5aa186462b4280ff92
Sameh Mohamed Ahmed Omr <Conan_Sameh@yahoo.com>
2021-09-30 03:07:35 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@
 export const environment = {
   production: false,
   apisVersion: "v1" ,
-  // baseUrl: 'http://boats-ride-backend.sameh.store',
-  baseUrl: 'https://backend.boatsride.com',
+   baseUrl: 'http://boats-ride-backend.sameh.store',
+ // baseUrl: 'https://backend.boatsride.com',
 
   url : function url (version = null) {
     return environment.baseUrl + '/api/' + ((version) ? version+'/' : '');
