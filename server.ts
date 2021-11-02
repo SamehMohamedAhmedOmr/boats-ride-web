2388ca4254bfffdae8a4a38b041a02c4d6ce6cd1
Sameh Mohamed Ahmed Omr <Conan_Sameh@yahoo.com>
2021-11-02 22:25:43 +0200

    fixes

--- server.ts
+++ server.ts
@@ -1,12 +1,12 @@
 import 'zone.js/dist/zone-node';
 
-import { ngExpressEngine } from '@nguniversal/express-engine';
+import {ngExpressEngine} from '@nguniversal/express-engine';
 import * as express from 'express';
-import { join } from 'path';
+import {join} from 'path';
 
-import { AppServerModule } from './src/main.server';
-import { APP_BASE_HREF } from '@angular/common';
-import { existsSync } from 'fs';
+import {AppServerModule} from './src/main.server';
+import {APP_BASE_HREF} from '@angular/common';
+import {existsSync} from 'fs';
 
 import 'localstorage-polyfill'
 
@@ -31,7 +31,7 @@ global['document'] = win.document;
 (global as any).branch = win.branch;
 (global as any).object = win.object;
 global['localStorage'] = localStorage;
-global['navigator'] = win.navigator ;
+global['navigator'] = win.navigator;
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
@@ -58,7 +58,7 @@ export function app(): express.Express {
 
   // All regular routes use the Universal engine
   server.get('*', (req, res) => {
-    res.render(indexHtml, { req, providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }] });
+    res.render(indexHtml, {req, providers: [{provide: APP_BASE_HREF, useValue: req.baseUrl}]});
   });
 
   return server;
@@ -80,8 +80,7 @@ function run(): void {
 declare const __non_webpack_require__: NodeRequire;
 const mainModule = __non_webpack_require__.main;
 const moduleFilename = mainModule && mainModule.filename || '';
-if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
-  run();
-}
+run();
+
 
 export * from './src/main.server';
