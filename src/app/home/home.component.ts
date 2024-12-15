6375ed6208eeb1229acb986deeac952590e36b7d
mustafa_mamdouh <mustafamammdoh@gmail.com>
2024-12-15 21:07:28 +0200

    finish add home section to home page from settings

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,8 @@ import {ChangeDetectorRef, Component, OnDestroy, OnInit, Inject, PLATFORM_ID} fr
 import {ToggleHeaderService} from '../../core/services/Helpers/toggle.header.service';
 import {SeoService} from "../../core/services/seo.service";
 import {isPlatformBrowser} from "@angular/common";
+import {SettingsService} from "../../core/services/settings.service";
+
 
 @Component({
   selector: 'app-home',
@@ -12,9 +14,12 @@ export class HomeComponent implements OnInit ,OnDestroy{
 
   isBrowser: boolean = false;
 
+  settings: any;
+
   constructor(private toggleHeader:ToggleHeaderService,
               private seoService: SeoService,
               @Inject(PLATFORM_ID) private platformId: any,
+              private settingsService: SettingsService,
               private cdr: ChangeDetectorRef) {
         this.isBrowser = isPlatformBrowser(platformId);
   }
@@ -22,6 +27,8 @@ export class HomeComponent implements OnInit ,OnDestroy{
   ngOnInit(): void {
     this.getSeo();
     this.toggleHeader.changeHeader(true);
+    this.settings = this.settingsService.getSettings();
+    console.log(this.settings);
   }
 
   ngOnDestroy(): void {
